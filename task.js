fd316c752adf0a00526e7c708a2966ee18c1cd86
SohamDas00 <jisusoham04@gmail.com>
2024-07-13 18:01:14 +0530

    Day1 complete

---
+++ task.js
@@ -0,0 +1,37 @@
+//Activity 1:
+var number=123;
+console.log(number);
+let string="Soham"
+console.log(string);
+
+//Activity 2:
+const bool=true;
+console.log(bool);
+
+//Activity3:
+let num=12345;
+let str="RAJA";
+let boole=false;
+let id={
+    name:"Soham",
+    class:"2nd Yr",
+    roll:12345,
+    sec:'C',
+};
+let arr=[12,46,"Rahul",25,"Soham",67];
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(boole));
+console.log(typeof(id));
+console.log(typeof(arr));
+
+//Activity 4:
+let variable=123;
+console.log(variable);
+variable=321;
+console.log(variable);
+
+//Activity 5:
+const variable2=12345;
+console.log(variable2);
+//If we reassign variable2 then it will show:- TypeError: Assignment to constant variable.
